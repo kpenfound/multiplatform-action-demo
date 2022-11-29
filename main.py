694e6236e3e38be69ac484b727ab798049b6556f
kpenfound <kyle@dagger.io>
2022-11-29 09:05:05 -0800

    update action version and script
    Signed-off-by: kpenfound <kyle@dagger.io>

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello from multiplatform-action-demo!")
+import os
+gh = os.getenv('GITHUB_ACTIONS')
+cci = os.getenv('CIRCLECI')
+location = 'local'
+if gh:
+  location = 'github actions'
+if cci:
+  location = 'cirlceci'
+print('Hello from multiplatform-action-demo in', location)
